71802abcfe0bf39c1d3c80e1a675644b83b0df7f
aw-andre <aw.andregerard@gmail.com>
2025-04-29 18:04:47 -0400

    fix: remove game name requirement if initializing or finalizing

--- src/cli.rs
+++ src/cli.rs
@@ -4,10 +4,6 @@ use std::fs;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
-    /// Game name
-    #[arg(long, required = true)]
-    pub game: String,
-
     /// Initialize database
     #[arg(long, required_unless_present_any = ["add", "delete", "finalize"])]
     pub initialize: bool,
@@ -16,10 +12,6 @@ pub struct Cli {
     #[arg(long, required_unless_present_any = ["initialize", "delete", "finalize"])]
     pub add: bool,
 
-    /// Files to parse
-    #[arg(long, required_if_eq("add", "true"), conflicts_with_all = ["initialize", "delete", "finalize"], num_args = 1, value_parser)]
-    pub files: Vec<String>,
-
     /// Delete database
     #[arg(long, required_unless_present_any = ["initialize", "add", "finalize"])]
     pub delete: bool,
@@ -27,6 +19,14 @@ pub struct Cli {
     /// Finalize database
     #[arg(long, required_unless_present_any = ["initialize", "add", "delete"])]
     pub finalize: bool,
+
+    /// Game name
+    #[arg(long, conflicts_with_all = ["initialize", "delete", "finalize"], required_unless_present_any = ["initialize", "finalize"])]
+    pub game: String,
+
+    /// Files to parse
+    #[arg(long, required_if_eq("add", "true"), conflicts_with_all = ["initialize", "delete", "finalize"], num_args = 1, value_parser)]
+    pub files: Vec<String>,
 }
 
 impl Cli {
